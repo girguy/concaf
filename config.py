b0509cc25ae1db16300ec05bf2f8453099077889
ggirineza <ggirineza@guygirinezaconsulting.onmicrosoft.com>
2024-01-18 17:41:51 +0100

    clean files and directory

---
+++ config.py
@@ -0,0 +1,29 @@
+TRANSFERTMARKT_TEAMS_ID = {
+    'Congo': '3854',
+    'Senegal': '3499',
+    'Mauritania': '14238',
+    'Maroc': '3575',
+    'Tanzania': '14666',
+    'Burkina Faso': '5872',
+    'Guinea': '3856',
+    'Gambia': '6186',
+    'Mali': '3674',
+    'Namibia': '3573',
+    'Algeria': '3614',
+    'Nigeria': '3444',
+    'Angola': '3585',
+    'Egypt': '3672',
+    'Tunisie': '3670',
+    'Equatorial Guinea': '13485',
+    'Ghana': '3441',
+    'Guinea-Bissau': '3701',
+    'Mozambique': '5129',
+    'Cameroon': '3434',
+    'South Africa': '3806',
+    'Ivory Coast': '3591',
+    'Zambia': '3703',
+    'Cape Verde': '4311',
+    'Ivory Coast': '3591'
+}
+
+PLAYER_FILE_NAME = 'player.csv'
